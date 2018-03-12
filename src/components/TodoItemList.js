83c30dfb05d89c40a91633a1ddf42f6af3394fb0
smallThinking <[wnsdnek778@gmail.com]>
2018-03-12 19:08:48 +0900

    18/03/12 commit

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 
 class TodoItemList extends Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.todos !== nextProps.todos;
+  }
+  
   render() {
     const { todos, onToggle, onRemove } = this.props;
 
     const todoList = todos.map(
-      (todo) => (
+      ({id, text, checked}) => (
         <TodoItem
-          {...todo}
+          id={id}
+          text={text}
+          checked={checked}
           onToggle={onToggle}
           onRemove={onRemove}
-          key={todo.id}
+          key={id}
         />
       )
     );
